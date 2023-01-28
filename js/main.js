8f5337dbc495e93494c24ff865888fa66dcc252a
LeWolfie <me@lewolfie.dev>
2023-01-28 15:53:50 -0600

    Multiple changes: Pop-ups and more!
    Added multiple pop-ups with object-oriented logic (thanks Joao!) to implement audio
    when clicked either the pop-up or a specified chapter; whereas Xenon Warriors'
    first chapter autoplays a video, and blurs when finished to urge the
    user for a Patreon subscription.
    Added new genres and their items; removed subscriptions.html with Patreon link.

--- js/main.js
+++ js/main.js
@@ -1,71 +1,149 @@
 function loadIn() {
-    setTimeout(function () {
-        var loadNextVideo = document.getElementById("loadNext");
+	setTimeout(function () {
+		var loadNextVideo = document.getElementById("loadNext");
 
-        fadeElement("Load Video", "loadNext", 1, 0, 0.1, 25, false);
+		fadeElement("Load Video", "loadNext", 1, 0, 0.1, 25, false);
 
-        setTimeout(function () {
-            loadNextVideo.classList.add("invisible");
-            //
-            var body = document.getElementById("body");
-            body.classList.remove("invisible");
-            //
-            fadeElement("Body", "body", 0, 1, 0.1, 50, true);
-        }, 500);
-    }, 4000);
+		setTimeout(function () {
+			loadNextVideo.classList.add("invisible");
+			//
+			var body = document.getElementById("body");
+			body.classList.remove("invisible");
+			//
+			fadeElement("Body", "body", 0, 1, 0.1, 50, true);
+		}, 500);
+	}, 4000);
 }
 
 function checkIfLoad() {
-    // Checks if a cookie called load has been stored in the browser
-    // If it does, doesn't load the transition.
-    const cookieValue = document.cookie
-        .split('; ')
-        .find((row) => row.startsWith('load='))
-        ?.split('=')[1];
+	// Checks if a cookie called load has been stored in the browser
+	// If it does, doesn't load the transition.
+	const cookieValue = document.cookie
+		.split('; ')
+		.find((row) => row.startsWith('load='))
+		?.split('=')[1];
 
-    if (cookieValue !== undefined) {
-        var body = document.getElementById("body");
-        var loadNextVideo = document.getElementById("loadNext");
-        loadNextVideo.classList.add("invisible");
-        body.classList.remove("invisible");
-    } else {
-        // Otherwise, adds the cookie
-        console.log("Added Load Cookie!");
-        document.cookie = "load=true; SameSite=strict; Secure";
-        loadIn();
-    }
+	if (cookieValue !== undefined) {
+		var body = document.getElementById("body");
+		var loadNextVideo = document.getElementById("loadNext");
+		loadNextVideo.classList.add("invisible");
+		body.classList.remove("invisible");
+	} else {
+		// Otherwise, adds the cookie
+		console.log("Added Load Cookie!");
+		document.cookie = "load=true; SameSite=strict; Secure";
+		loadIn();
+	}
 }
 
-function dead() {
-    var body = document.getElementsByTagName("body")[0];
-	body.style.overflow = "hidden";
-	var pop = document.getElementById("popup");
+function popup(id) {
+	var body = document.getElementsByTagName("body")[0];
+
+	if (body.style.overflow !== "hidden")
+		body.style.overflow = "hidden";
+	else
+		body.style.overflow = "visible";
+
+	var pop = document.getElementById(id);
 	pop.classList.toggle("visible");
-	var music = new Audio("../resources/isolated.mp3");
-	music.volume = 0.2;
-	music.fastSeek(15);
-	music.play();
 }
 
+var items = {
+	dead: {
+		elementId: "dead planet",
+		popupId: "popup-dead",
+		songName: "isolated"
+	},
+	xenon: {
+		elementId: "xenon warriors",
+		popupId: "popup-xenon",
+	}
+}
+
+var songs = {
+	isolated: {
+		audio: new Audio("../resources/isolated.mp3"),
+		volume: 0.2,
+		fastSeek: 15
+	},
+	thunder: {
+		audio: new Audio("../resources/thunder_music.mp3"),
+		volume: 0.05
+	}
+}
+
+function onItemClicked(name, obj) {
+	console.log("Item clicked: " + name);
+	popup(obj.popupId);
+
+	if (typeof obj.songName !== "undefined") {
+		let song = songs[obj.songName];
+		let audio = song.audio;
+
+		if (audio.paused) {
+			if (song.volume !== undefined) audio.volume = song.volume;
+			if (song.fastSeek !== undefined) audio.fastSeek(song.fastSeek);
+			audio.play();
+		} else {
+			audio.pause();
+		}
+	}
+}
+
+function start() {
+	checkIfLoad();
+	loadIn();
+
+	speaker(fadeAudio('../resources/1_1.mp3'));
+
+	// Fades out image by 0.025 every 100ms from 1 to 0
+	fadeElement("Image", "banner", 1, 0, 0.025, 100, false);
+
+	// Fades in video by 0.05 every 200ms from 0 to 1
+	fadeElement("Video", "video", 0, 1, 0.05, 200, true);
+}
+
+window.onload = async function () {
+	for (let item in items) {
+		let obj = items[item];
+		obj.element = document.getElementById(obj.elementId);
+		obj.element.onclick = () => onItemClicked(item, obj);
+	}
+
+	let xenonChapter = document.getElementById("chapter-1");
+	xenonChapter.onclick = () => {
+		let boom = document.getElementById("boom");
+		boom.classList.toggle("invisible");
+		boom.play();
+
+		songs.thunder.audio.volume = 0.05;
+		songs.thunder.audio.play();
+
+		let xenon = document.getElementById(items.xenon.popupId);
+		xenon.style.top = "-50%";
+		console.log(xenon.style.top);
+
+		var interval = setInterval(loop, 1000);
+
+		function loop() {
+			var duration = boom.duration;
+			var time = boom.currentTime;
+			if (duration == time) {
+				console.log("Finished!");
+				boom.style.filter = "blur(.5em)";
+				songs.thunder.pause();
+				clearInterval(interval);
+			}
+			console.log("Here!");
+		}
+	}
+}
+
+/*
 function jinrui() {
 	var main = document.getElementById("main");
 	main.classList.toggle("blur");
 	var pop = document.getElementById("popup");
 	pop.classList.toggle("active");
-
 }
-
-window.onload = async function () {
-	checkIfLoad();
-	loadIn();
-    speaker(fadeAudio('../resources/1_1.mp3'));
-
-    // Fades out image by 0.025 every 100ms from 1 to 0
-    fadeElement("Image", "banner", 1, 0, 0.025, 100, false);
-
-    // Fades in video by 0.05 every 200ms from 0 to 1
-    fadeElement("Video", "video", 0, 1, 0.05, 200, true);
-
-	let deadElement = document.getElementById("dead planet");
-	deadElement.onclick = dead;
-}
+*/
